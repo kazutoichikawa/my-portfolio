eecc0b6cc6837749e3e0db2e3a36e91a7b43d0ab
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-03-12 13:17:26 +0900

    ルートコンポーネントの生成

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <header>
       <h2>My Portfolio</h2>
-      <ul>
+      <ul class="contents">
         <li><router-link to="/profile">Profile</router-link></li>
         <li><router-link to="/language">Languages</router-link></li>
-        <li><router-link to="/work">Work</router-link></li>
+        <li><router-link to="/work">Works</router-link></li>
       </ul>
     </header>
     <router-view/>
@@ -25,5 +25,22 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.contents {
+  margin: 20px 100px;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+
+h2 {
+  font-size: 40px;
+}
+
+a {
+  font-weight: bold;
+  text-decoration: none;
+  font-size: 25px;
 }
 </style>
